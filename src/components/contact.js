0339ff1fb80a05faa42ddf095f23b5ee63af611c
bokamix <janiakpol@gmail.com>
2019-06-26 17:25:12 +0200

    add contact sector

--- src/components/contact.js
+++ src/components/contact.js
@@ -1,11 +1,20 @@
 import React from "react"
-import contactStyles from "./contact.module.scss"
+import contactStyles from "./contact.module.scss";
+import profileImg from "../images/gatsby-icon.png";
 
 const contact = () => {
 return (
   <>
   <h2>Contact</h2>
-  <div className={contactStyles.container}></div>
+  <div className={contactStyles.container}>
+    <div className={contactStyles.box}>
+      <h2>Contact with me:</h2>
+      <p>LinkedIn Profile: Link</p>
+      <p>Facebook Messenger: Link</p>
+      <p>Email: jededesign@gmail.com</p>
+    </div>
+    <div className={contactStyles.box}><img className={contactStyles.avatar} src={profileImg} width="200" alt="Author" /></div>
+  </div>
   </>
 )
 
--- src/components/footer.js
+++ src/components/footer.js
@@ -1,9 +1,14 @@
 import React from "react"
 import styleFooter from "./footer.module.scss";
 
+
+
+
 const Footer = () => {
 return (
     <div className={styleFooter.container}>
+
+      
 <div className={styleFooter.box}>Tak to już jest</div>
 <div className={styleFooter.box}>Tak to już jest</div>
 <div className={styleFooter.box}>Tak to już jest</div>   
