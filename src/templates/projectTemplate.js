09a7bdcde3d3625703721365b465cc867d341640
bokamix <janiakpol@gmail.com>
2019-06-26 15:55:25 +0200

    add system who change img on project

--- src/templates/projectTemplate.js
+++ src/templates/projectTemplate.js
@@ -5,20 +5,41 @@ import Layout from "../components/layout";
 
 
 
+var x=1;
 
 const projectTemplate = ({pageContext:{ data }}) => (
   <>
     
     <Layout>
     <div className={templateStyles.container}>
+  <button onClick={()=>changeimg(data)} id="buttonM">dasd</button>
   <h1>{data.name}</h1>
   <p>{data.about}</p>
-  <img className={templateStyles.project__img__desktop}src={data.screendesktop.url} alt={'Image' + data.name} />
+  <img id="project__img" className={templateStyles.project__img__desktop}src={(x===1 && data.screendesktop.url) || 0} alt={'Image' + data.name} />
   </div>
     </Layout>
 
     
   </>
 )
+
+
+function changeimg(data){
+  let imgscreenvalue = document.querySelector("#project__img");
+    if(x === 1)
+    {
+      
+      imgscreenvalue.src = data.imgscreen.url      
+      x=2
+    }
+else if( x === 2){
+
+  
+  imgscreenvalue.src = data.screendesktop.url 
+  x=1
+}
+
+  }
+
 
 export default projectTemplate
