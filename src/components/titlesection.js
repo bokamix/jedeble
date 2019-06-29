26c8d3905bc062283f134c096373fa904bbf165c
bokamix <janiakpol@gmail.com>
2019-06-29 19:34:55 +0200

    Styling statistic content

--- src/components/titlesection.js
+++ src/components/titlesection.js
@@ -14,34 +14,44 @@ return (
   </div>
   <h2>Jakie są najpopularniejsze CMS?</h2>
   <div className={TitleSectionStyle.statisctcs__container}>
-  <div>Wordpress 59% </div>
-  <div>Joomla 6.7%</div>
-  <div>Drupal 4.7%</div>
-  <div>Magento 2.3%</div>
+ <div><p>Wordpress 59% </p>
+  <p>Joomla 6.7%</p>
+  <p>Drupal 4.7%</p>
+  <p>Magento 2.3%</p></div>
+  <div>
+    <h3>to to tam</h3>
   <p>Data from 2018: BuiltWith.com, W3tech.com, SimilarTech, Google Trends.</p>
+  </div>
+  
   </div>
 
   <h2>Gdzie ludzie używają internetów?</h2>
   <div className={TitleSectionStyle.statisctcs__container}>
-  <div>64% Mobile</div>
-  <div>57% desktop</div>
-  <div>45% Social media user</div>
-  <div>43% Mobile social media user</div>
+  <div>
+    <h3>BlaBla Bla</h3>
   <p>data from wearesocial.com, hootsuite.com</p>
+  </div>
+    <div className={TitleSectionStyle.value__content}>
+      <span>64%</span> <p> Mobile</p>
+      <span>57% </span><p>desktop</p>
+      <span>45% </span><p>Social media user</p>
+      <span>43% </span> <p>Mobile social media user</p>
+    </div>
+
+
   </div>
 
   <h2>Co ich wkurza??</h2>
   <div className={TitleSectionStyle.statisctcs__container}>
-      <div>DŁugi czas wczytywania strony</div>
-      <div>Reklamy</div>
-      <div>Wyskakujące okienka</div>
-      <div>Powtarzające się treści</div>
+    <div> <p>Długi czas wczytywania strony</p>
+      <p>Reklamy</p>
+      <p>Wyskakujące okienka</p>
+      <p>Powtarzające się treści</p></div>
+     <div><h2>takie tam sobie rzeczy</h2><p>sijdasdoasjdoijsaoi jsdoiasjdoi</p></div>
   </div>
 
   <h2>Rozwiązanie na niektóre problemy</h2>
-  <p>Internetu nie naprawimy, ale możemy go przyśpieszyć i zadbać o jego większe bezpieczeństwo.
-    
-  </p>
+  <p>Internetu nie naprawimy, ale możemy go przyśpieszyć i zadbać o jego większe bezpieczeństwo.  </p>
   <div className={TitleSectionStyle.statisctcs__container}>
   <div>Bezpieczny CMS</div>
   <div>Mobile first website</div>
