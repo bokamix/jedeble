80ee6acf3c34ab29993b90bc2bc5a5a161a11800
bokamix <janiakpol@gmail.com>
2019-07-21 20:38:45 +0200

    add elements who car

--- src/components/about.js
+++ src/components/about.js
@@ -1,18 +1,47 @@
 import React from "react"
 import aboutStyles from "./about.module.scss"
 
-
+import photoshop from "../images/photoshop.png";
+import uxpin from "../images/uxpin.png";
+import bootstrap from "../images/bootstrap.png";
+import jsIcon from "../images/javascript.png";
+import GatsbyIcon from "../images/gatsby.png";
+import reactLogo from "../images/reactjs.png";
+import netlifyLogo from "../images/netlify.png";
+import NPMlogo from "../images/npm.png";
+import gitLogo from "../images/git.png";
+import JAMStackLogo from "../images/jamstack.png";
+import GraphCMS from "../images/graphcms.png";
+import GraphQLLogo from "../images/graphql.png";
+import Chatfuel from "../images/chatfuel.png";
+import Ableton from "../images/ableton.png";
+import SonyVegasPro from "../images/sony-vegas-pro.png";
+import FacebookADS from "../images/facebook-ads.png"
 
  
 const About = () => {
 return (
   <>
-  <div className={aboutStyles.section}>
+  <div className={aboutStyles.contact__section}>
 
  
 
   <div className={aboutStyles.wrapper__logos}>
   <h2>Technology</h2>
+<div className={aboutStyles.technology__logos__mobile}>
+<div className={aboutStyles.box}><div className={aboutStyles.wrapper__logos__mobile}><img alt= "uxpin" src={uxpin}  /><img src={photoshop} alt="photoshop" /><img alt="bootstrap" src={bootstrap}  /></div></div>
+<div className={aboutStyles.box}><div className={aboutStyles.wrapper__logos__mobile}>
+<img alt= "JavaScript Icon" src={jsIcon}  /><img alt= "Gatsby" src={GatsbyIcon}  /><img alt= "ReactJs Icon" src={reactLogo}  />
+<img alt= "Netlify Logo" src={netlifyLogo}  /><img alt= "NPM" src={NPMlogo} width="130" />
+<img alt= "Git Logo" src={gitLogo}  /><img alt= "JAMStack" src={JAMStackLogo}  /><img alt= "GraphCMS" src={GraphCMS}  />
+<img alt= "GraphQL" src={GraphQLLogo}  />
+  </div></div>
+<div className={aboutStyles.box}><div className={aboutStyles.wrapper__logos__mobile}>
+  <img alt= "Facebook Ads" src={FacebookADS}  />
+  <img alt= "Ableton Live" src={Ableton}  />
+  <img alt= "Sony Vegas Pro" src={SonyVegasPro}  />
+  <img alt= "Chatfuel" src={Chatfuel}  /></div>
+  </div></div>
     </div></div>
 
 
--- src/components/contact.js
+++ src/components/contact.js
@@ -5,12 +5,12 @@ import profileImg from "../images/gatsby-icon.png";
 const contact = () => {
 return (
   <>
-  <h2>Contact</h2>
+  <h2 className={contactStyles.section__title}>Contact</h2>
   <div className={contactStyles.container}>
     <div className={contactStyles.box}>
       <h2>Contact with me:</h2>
-      <p>LinkedIn Profile (<a target="_blank" href="https://www.linkedin.com/in/wojciech-janiak-7a7808136/">click</a>)</p>
-      <p>Facebook Messenger (<a target="_blank" href="https://www.m.me/jedepl">click</a>)</p>
+      <p>LinkedIn Profile (<a rel="noopener noreferrer" target="_blank" href="https://www.linkedin.com/in/wojciech-janiak-7a7808136/">click</a>)</p>
+      <p>Facebook Messenger (<a rel="noopener noreferrer" target="_blank" href="https://www.m.me/jedepl">click</a>)</p>
       <p>Email: jededesign@gmail.com</p>
     </div>
     <div className={contactStyles.box}><img className={contactStyles.avatar} src={profileImg} width="200" alt="Author" /></div>
