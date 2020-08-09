091f90ba9a36a84be03d926d9f83117ab345f143
Michał Skrzypczyński <justflow555@gmail.com>
2020-08-09 19:28:22 +0200

    Add mordo mordooo

--- src/components/about.js
+++ src/components/about.js
@@ -19,44 +19,138 @@ import SonyVegasPro from "../images/sony-vegas-pro.png"
 import FacebookADS from "../images/facebook-ads.png"
 
 const About = () => {
-  return (
-    <>
-      <div className={aboutStyles.contact__section}>
-        <div className={aboutStyles.wrapper__logos}>
-          <h2>Technology</h2>
-          <div className={aboutStyles.technology__logos__mobile}>
-            <div className={aboutStyles.box}>
-              <div className={aboutStyles.wrapper__logos__mobile}>
-                <img alt="uxpin" src={uxpin} />
-                <img src={photoshop} alt="photoshop" />
-                <img alt="bootstrap" src={bootstrap} />
-              </div>
-            </div>
-            <div className={aboutStyles.box}>
-              <div className={aboutStyles.wrapper__logos__mobile}>
-                <img alt="JavaScript Icon" src={jsIcon} />
-                <img alt="Gatsby" src={GatsbyIcon} />
-                <img alt="ReactJs Icon" src={reactLogo} />
-                <img alt="Netlify Logo" src={netlifyLogo} />
-                <img alt="NPM" src={NPMlogo} width="130" />
-                <img alt="Git Logo" src={gitLogo} />
-                <img alt="JAMStack" src={JAMStackLogo} />
-                <img alt="GraphCMS" src={GraphCMS} />
-                <img alt="GraphQL" src={GraphQLLogo} />
-              </div>
-            </div>
-            <div className={aboutStyles.box}>
-              <div className={aboutStyles.wrapper__logos__mobile}>
-                <img alt="Facebook Ads" src={FacebookADS} />
-                <img alt="Ableton Live" src={Ableton} />
-                <img alt="Sony Vegas Pro" src={SonyVegasPro} />
-                <img alt="Chatfuel" src={Chatfuel} />
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </>
+  return ( <
+    >
+    <
+    div className = {
+      aboutStyles.contact__section
+    } >
+    <
+    div className = {
+      aboutStyles.wrapper__logos
+    } >
+    <
+    h2 > Technology < /h2> <
+    div className = {
+      aboutStyles.technology__logos__mobile
+    } >
+    <
+    div className = {
+      aboutStyles.box
+    } >
+    <
+    div className = {
+      aboutStyles.wrapper__logos__mobile
+    } >
+    <
+    img alt = "uxpin"
+    src = {
+      uxpin
+    }
+    /> <
+    img src = {
+      photoshop
+    }
+    alt = "photoshop" / >
+    <
+    img alt = "bootstrap"
+    src = {
+      bootstrap
+    }
+    /> <
+    /div> <
+    /div> <
+    div className = {
+      aboutStyles.box
+    } >
+    <
+    div className = {
+      aboutStyles.wrapper__logos__mobile
+    } >
+    <
+    img alt = "JavaScript Icon"
+    src = {
+      jsIcon
+    }
+    /> <
+    img alt = "Gatsby"
+    src = {
+      GatsbyIcon
+    }
+    /> <
+    img alt = "ReactJs Icon"
+    src = {
+      reactLogo
+    }
+    /> <
+    img alt = "Netlify Logo"
+    src = {
+      netlifyLogo
+    }
+    /> <
+    img alt = "NPM"
+    src = {
+      NPMlogo
+    }
+    width = "130" / >
+    <
+    img alt = "Git Logo"
+    src = {
+      gitLogo
+    }
+    /> <
+    img alt = "JAMStack"
+    src = {
+      JAMStackLogo
+    }
+    /> <
+    img alt = "GraphCMS"
+    src = {
+      GraphCMS
+    }
+    /> <
+    img alt = "GraphQL"
+    src = {
+      GraphQLLogo
+    }
+    /> <
+    /div> <
+    /div> <
+    div className = {
+      aboutStyles.box
+    } >
+    <
+    div className = {
+      aboutStyles.wrapper__logos__mobile
+    } >
+    <
+    img alt = "Facebook Ads"
+    src = {
+      FacebookADS
+    }
+    /> <
+    img alt = "Ableton Live"
+    src = {
+      Ableton
+    }
+    /> <
+    img alt = "Sony Vegas Pro"
+    src = {
+      SonyVegasPro
+    }
+    /> <
+    img alt = "Chatfuel"
+    src = {
+      Chatfuel
+    }
+    /> <
+    p > VUE JS bruh < /p> <
+    /div> <
+    /div> <
+    /div> <
+    /div> <
+    /div> <
+    />
   )
 }
 
